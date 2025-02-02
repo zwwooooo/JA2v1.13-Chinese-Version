89b3fb2c4a051babef2867ceb0648b0f2f9343f0
zwwooooo <zwwblog@gmail.com>
2025-02-02 12:41:38 +0800

    TypeP 原文件

---
+++ Data-TypeP_2014/Scripts/undergroundsectornames.lua
@@ -0,0 +1,58 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["G10-1"] = "Azkaban Dungeon",
+    ["G10-2"] = "Azkaban Dungeon",
+    
+    ["A9-1"] = "Nasa Basement",
+        
+    ["A1-1"] = "Shelter",
+    
+    
+    ["K2-1"] = "Duranth Mine",
+	["K1-1"] = "Duranth Mine",
+	["K1-2"] = "Duranth Mine",
+	["K1-3"] = "Duranth Mine",
+	  
+    ["J10-1"] = "LodenBurg Mine",
+	["J11-1"] = "LodenBurg Mine",
+	["J11-2"] = "LodenBurg Mine",
+	["J11-3"] = "LodenBurg Mine",
+    
+    ["D11-1"] = "Alaspar Mine",
+	["E11-1"] = "Alaspar Mine",
+	["E11-2"] = "Alaspar Mine",
+	["E11-3"] = "Alaspar Mine",
+ 
+    ["F14-1"] = "Drasnia Mine",
+	["F15-1"] = "Drasnia Mine",
+	["F16-1"] = "Drasnia Mine",
+	["F16-2"] = "Drasnia Mine",
+	["F16-3"] = "Drasnia Mine",
+
+    ["O5-1"] = "Quevelin Mine",
+	["O6-1"] = "Quevelin Mine",
+	["O6-2"] = "Quevelin Mine",
+	["O6-3"] = "Quevelin Mine",
+    
+    ["H5-1"] = "Aspenwood Mine",
+	["H4-1"] = "Aspenwood Mine",
+	["H4-2"] = "Aspenwood Mine",
+	["H4-3"] = "Aspenwood Mine",
+
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "Creature Lair",
+}
