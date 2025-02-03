0f1baffd5b156131f252ddea4c7fbae5bba2107d
zwwooooo <zwwblog@gmail.com>
2025-02-03 11:21:16 +0800

    241117v0.2 。增加需要翻译的lua文件

---
+++ Data-TypeP_2014_ChsPatch/Scripts/Chinese.undergroundsectornames.lua
@@ -0,0 +1,58 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["G10-1"] = "Azkaban地牢",
+    ["G10-2"] = "Azkaban地牢",
+    
+    ["A9-1"] = "Nasa地下室",
+        
+    ["A1-1"] = "避难所",
+    
+    
+    ["K2-1"] = "Duranth矿场",
+    ["K1-1"] = "Duranth矿场",
+    ["K1-2"] = "Duranth矿场",
+    ["K1-3"] = "Duranth矿场",
+      
+    ["J10-1"] = "LodenBurg矿场",
+    ["J11-1"] = "LodenBurg矿场",
+    ["J11-2"] = "LodenBurg矿场",
+    ["J11-3"] = "LodenBurg矿场",
+    
+    ["D11-1"] = "Alaspar矿场",
+    ["E11-1"] = "Alaspar矿场",
+    ["E11-2"] = "Alaspar矿场",
+    ["E11-3"] = "Alaspar矿场",
+ 
+    ["F14-1"] = "Drasnia矿场",
+    ["F15-1"] = "Drasnia矿场",
+    ["F16-1"] = "Drasnia矿场",
+    ["F16-2"] = "Drasnia矿场",
+    ["F16-3"] = "Drasnia矿场",
+
+    ["O5-1"] = "Quevelin矿场",
+    ["O6-1"] = "Quevelin矿场",
+    ["O6-2"] = "Quevelin矿场",
+    ["O6-3"] = "Quevelin矿场",
+    
+    ["H5-1"] = "Aspenwood矿场",
+    ["H4-1"] = "Aspenwood矿场",
+    ["H4-2"] = "Aspenwood矿场",
+    ["H4-3"] = "Aspenwood矿场",
+
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "异形巢穴",
+}
